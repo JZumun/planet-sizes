1d598b66bf4187f5908006bf809c3210a4901774
John Reinier Bulaong <rein.bulaong@yahoo.com>
2025-05-10 21:31:02 +0800

    allow horizontal scrolling on gallery

--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -1,22 +1,35 @@
 <template>
-  <section class="celestial-bodies-gallery" v-bind="$attrs">
+  <section class="celestial-bodies-gallery" v-bind="$attrs" ref="main" @wheel="scroll">
     <div class="scale-container">
       <div class="scale">{{ Intl.NumberFormat().format(scale * 100) }} km</div>
       <p class="scale-disclaimer" v-if="!showScene">Distances between objects not drawn to scale</p>
     </div>
-    <celestial-scene v-if="showScene" :scene="scene!.system!" :scale="scale" :showName="showNames"
-      :showDiameter="showDiameters" />
+    <celestial-scene
+      class="celestial-bodies"
+      v-if="showScene"
+      :scene="scene!.system!"
+      :scale="scale"
+      :showName="showNames"
+      :showDiameter="showDiameters"
+    />
     <template v-else>
       <section class="celestial-bodies" v-if="bodies.length > 0">
         <transition-group name="fade">
-          <celestial-body class="celestial-body-item" v-for="body in bodies" v-bind:key="body.key" :id="body.key"
-            :body="body" :scale="scale" :showName="showNames" :showDiameter="showDiameters"
-            :showGroups="bodies.length == 1" />
+          <celestial-body
+            class="celestial-body-item"
+            v-for="body in bodies"
+            v-bind:key="body.key"
+            :id="body.key"
+            :body="body"
+            :scale="scale"
+            :showName="showNames"
+            :showDiameter="showDiameters"
+            :showGroups="bodies.length == 1"
+          />
         </transition-group>
       </section>
       <section class="empty-message" v-else>Choose which celestial bodies to display from the options.</section>
     </template>
-
   </section>
   <teleport to="#sidebar">
     <explore-panel :scene="scene?.key" :bodies="bodies" />
@@ -32,16 +45,19 @@
 
       <template v-if="supportsAccurateDistances">
         <label for="show-accurate-distances">Show Accurate Distances <span class="tag">(experimental)</span></label>
-        <input type="checkbox" name="show-accurate-distances" id="show-accurate-distances"
-          v-model="showAccurateDistances" />
+        <input
+          type="checkbox"
+          name="show-accurate-distances"
+          id="show-accurate-distances"
+          v-model="showAccurateDistances"
+        />
       </template>
-
     </sidebar-panel>
   </teleport>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, ref, useTemplateRef } from "vue";
 import { Body, Scene } from "../../data/data";
 import CelestialBody from "./CelestialBody.vue";
 import ExplorePanel from "./ExplorePanel.vue";
@@ -51,9 +67,8 @@ import CelestialScene from "./Scene.vue";
 
 const props = defineProps<{
   scene?: Scene;
-  bodies: Body[]
+  bodies: Body[];
 }>();
-
 
 const zoom = ref(1);
 const showNames = ref(true);
@@ -62,32 +77,37 @@ const showAccurateDistances = ref(true);
 
 const supportsAccurateDistances = computed(() => {
   return props.scene?.system != undefined;
-})
+});
 const showScene = computed(() => {
   return supportsAccurateDistances.value && showAccurateDistances.value;
-})
+});
 
 const initialScale = computed(() => {
   if (showScene.value) {
     if (props.scene!.system!.suggested_scale) {
       return props.scene!.system!.suggested_scale / 8 / 100;
     }
-    const largestDistance = Object.values(props.scene!.system!.satellites)
-      .reduce((max, body) => Math.max(max, body.distance), 0)
+    const largestDistance = Object.values(props.scene!.system!.satellites).reduce(
+      (max, body) => Math.max(max, body.distance),
+      0
+    );
     return largestDistance / 8 / 100;
   }
 
-  const largestBodyRadius = props.bodies.reduce(
-    (max, body) => Math.max(max, body.radius[0]),
-    0
-  );
+  const largestBodyRadius = props.bodies.reduce((max, body) => Math.max(max, body.radius[0]), 0);
 
   return Math.max(1, largestBodyRadius / 100);
 });
 
-const scale = computed(() =>
-  Math.max(1, Math.floor((initialScale.value / zoom.value) * 10) / 10)
-);
+const scale = computed(() => Math.max(1, Math.floor((initialScale.value / zoom.value) * 10) / 10));
+
+const galleryRef = useTemplateRef("main");
+function scroll(e: WheelEvent) {
+  const item = galleryRef.value;
+  if (item == null) return;
+  if (e.deltaY > 0) item.scrollLeft! += 100;
+  else item.scrollLeft -= 100;
+}
 </script>
 
 <style scoped>
